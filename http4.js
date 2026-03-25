00c936a9f942ed7570840990ce89db81a91a0aab
Bansi12439 <bansikhunt02@gmail.com>
2026-03-25 09:10:15 +0530

    Add files via upload

---
+++ http4.js
@@ -0,0 +1,20 @@
+var http=require("http")
+http.createServer((req,res)=>{
+    if(req.url==="/"){
+        res.end(`<h1>style="font-size:45px text-align:center color:red"
+            welcome to my website</h1>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>`)
+    }
+    else if(req.url==="/about")
+    {
+        res.end()
+    }
+    else if(req.url==="/contact")
+    {
+        res.end()
+    }
+    else{
+        res.end("page not found");
+    }
+}).listen(7007)
