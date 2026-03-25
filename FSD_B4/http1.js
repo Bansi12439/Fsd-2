6a2ea307eba3ef227b3db1e55bea62e0642417be
Bansi12439 <bansikhunt02@gmail.com>
2026-03-25 09:27:18 +0530

    Delete FSD_B4 directory

--- FSD_B4/http1.js
+++
@@ -1,8 +0,0 @@
-var http=require("http")
-http.createServer((req,res)=>{
-    res.writeHead(200,{"content-type":"text/html"})
-    res.end("<h1>HTTP Module Example</h1>")
-    res.write("hiii")
-}).listen(7008,()=>{
-    console.log("Server started https://localhost:7008")
-})
--- FSD_B4/http2.js
+++
@@ -1,6 +0,0 @@
-var http=require("http")
-http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"application/json"})
-    var d={"sub":"FSD-2","faculty":"NAS"}
-    res.end(JSON.stringify(d))
-}).listen(7001)
